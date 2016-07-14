fe95fd23418b2a7c88c5bfd0ca9bc29055c2b1d2
nikklein <objcoding@gmail.com>
2016-07-14 10:38:03 +0100

    Traffic light challenge

--- four-phase-testing/traffic_light/spec/traffic_light_spec.rb
+++ four-phase-testing/traffic_light/spec/traffic_light_spec.rb
@@ -1,15 +1,33 @@
 require_relative '../lib/traffic_light.rb'
 
+#The TrafficLight class is responsible to show the state of Traffic Light
 describe TrafficLight do
+let(:subject) {described_class.new}
 
-  it 'this is a fantastic test' do
-#setup
-    x = 1
-#exercise
-    x += 1
-#verify
-    expect(x).to eq(2)
-#teardown (you will probably not need this)
-  end
+  it 'checks that initially traffic light object state is stop' do
+expect(subject.state).to eq :stop
+end
+describe '#next' do
+
+it 'change its state from :stop to :wait' do
+expect(subject.next).to eq :wait
+end
+it 'change its state from :wait to :go' do
+subject.next
+expect(subject.next).to eq :go
+end
+it 'change its state from :go to :wait' do
+subject.next
+subject.next
+expect(subject.next).to eq :wait
+end
+it 'change its state from :wait to :stop if previous state was :go' do
+subject.next
+subject.next
+subject.next
+expect(subject.next).to eq :stop
+end
+end
+
 
 end
